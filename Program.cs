8eadd20c5b43a90019bf338418e7ed96177d4752
tweaky <gleb2604@bk.ru>
2022-12-29 18:51:49 +0300

    Сделали 23 задачу

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.Clear();
+// Необходимо учесть отрицательное введение число.
+Console.WriteLine("Введите число от 1 до N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Таблица кубов чисел от 1 до {number}:");
+Console.WriteLine("------------------------------");
+
+/*
+// 1. Через цикл for:
+for (int x = 1; x <=number; x++)
+    {
+        if(number < 1)
+            {
+                Console.WriteLine("Ошибка.Введите число от 1 до N");
+                return;
+            }
+        int result = x*x*x;
+        Console.WriteLine($"{x}*{x}*{x} = {result}");
+   }
+*/
+
+/*
+// 2. Через цикл  while:
+int x = 1;
+while (x <= number)
+{
+    if(number < 1)
+        {
+           Console.WriteLine("Ошибка.Введите число от 1 до N");
+           return;
+        }
+    int result = x*x*x;
+    Console.WriteLine($"{x}*{x}*{x} = {result}");
+    x++;
+}
+*/
+
+/*
+// 3. Через цикл for через Math:
+for (int x = 1; x <=number; x++)
+    {
+        if(number < 1)
+            {
+                Console.WriteLine("Ошибка.Введите число от 1 до N");
+                return;
+            }
+        Console.Write($"{x}*{x}*{x} = ");
+        Console.WriteLine(Math.Pow(x, 3));
+   }
+*/
+
+/*
+// 4. Через цикл  while через Math:
+int x = 1;
+while (x <= number)
+{
+    if(number < 1)
+        {
+            Console.WriteLine("Ошибка.Введите число от 1 до N");
+            return;
+        }
+    Console.Write($"{x}*{x}*{x} = ");
+    Console.WriteLine(Math.Pow(x, 3));
+    x++;
+}
+*/
